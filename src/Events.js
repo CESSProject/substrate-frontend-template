375606c034235f3626479d772bc03f1312826803
Jimmy Chu <jimmychu0807@gmail.com>
2023-09-28 08:19:27 +0800

    Event module fixed on handling multiple useEffect() firing

--- src/Events.js
+++ src/Events.js
@@ -4,9 +4,7 @@ import { Feed, Grid, Button } from "semantic-ui-react";
 import { useSubstrateState } from "./substrate-lib";
 
 // Events to be filtered out from the event feed
-const FILTERED_EVENTS = [
-  'system:ExtrinsicSuccess',
-];
+const FILTERED_EVENTS = ["system:ExtrinsicSuccess"];
 
 const eventName = (ev) => `${ev.section}:${ev.method}`;
 const eventParams = (ev) => JSON.stringify(ev.data);
@@ -14,31 +12,22 @@ const eventParams = (ev) => JSON.stringify(ev.data);
 function Main(props) {
   const { api } = useSubstrateState();
   const [eventFeed, setEventFeed] = useState([[], new Set()]);
-  const [subscribed, setSubscribed] = useState(false);
 
   useEffect(() => {
     let unsub = null;
 
     const allEvents = async () => {
-      console.log("subscribed");
-
       unsub = await api.query.system.events((events) => {
-
-        console.log("events received, with len:", events.length);
-
         const feed = events
           .map((record) => {
             // extract the phase, event and the event types
             const evHuman = record.event.toHuman();
             const evName = eventName(evHuman);
             const evParams = eventParams(evHuman);
-
-            console.log("evHuman", evHuman);
-
             return { evName, evParams };
           })
           .filter(({ evName }) => !FILTERED_EVENTS.includes(evName))
-          .map(({evName, evParams}, idx) => ({
+          .map(({ evName, evParams }, idx) => ({
             key: `${eventFeed[0].length + idx}-${evName}`,
             icon: "bell",
             summary: evName,
@@ -50,33 +39,25 @@ function Main(props) {
         setEventFeed(([prevFeed, prevSet]) => {
           // Because React fires useEffect() twice in strict mode, we need to ensure the events
           // haven't been added to the event feed before.
-          console.log("prevFeed", prevFeed);
-          console.log("prevSet", prevSet);
-
           const newFeed = feed.filter((oneFeed) => !prevSet.has(oneFeed.key));
+          if (newFeed.length === 0) return [prevFeed, prevSet];
 
-          if (newFeed.length === 0) return ([prevFeed, prevSet]);
-
-          // const newSet = new Set(Array.from(prevSet));
+          // Construct the newSet
           const newSet = new Set(prevSet);
           newFeed.forEach((oneFeed) => newSet.add(oneFeed.key));
-
-          console.log("addedFeed", newFeed);
-          console.log("newSet", newSet);
-
-          return([[...newFeed, ...prevFeed], newSet]);
+          return [[...newFeed, ...prevFeed], newSet];
         });
       });
-      setSubscribed(true);
     };
 
-    !subscribed && allEvents();
-    return (() => {
+    // We only want to call the event subscription fn if it hasn't previously subscribed.
+    !unsub && allEvents();
+
+    return () => {
       unsub && unsub();
-      console.log("UNsubscribed");
-      setSubscribed(false);
-    })
-  }, [api.query.system]);
+      unsub = null;
+    };
+  }, [api.query.system, eventFeed]);
 
   const { feedMaxHeight = 250 } = props;
 
